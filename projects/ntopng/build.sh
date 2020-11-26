7797ca9833cbfc4dbff533ee59ce3da783267a4b
Thomas Forrer <thomas.forrer@wuerth-phoenix.com>
2020-11-26 15:32:02 +0100

    add patch for Makefile and fix build

--- projects/ntopng/build.sh
+++ projects/ntopng/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+# apply patches
+patch -p0 < Makefile.in.patch
+
+# build nDPI project
+cd nDPI
+sh autogen.sh
+./configure
+make
+cd ..
+
+# build ntopng project
+cd ntopng
+sh autogen.sh
+./configure
+make
+cd ..
+
+# build fuzzers
 
